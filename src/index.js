4a778244f8b598c29468df206315fae2aa098750
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-08-22 16:11:14 +0300

    add-refs

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
+const searchInput = document.querySelector(`input`)
+const searchBtn = document.querySelector(`.search-btn`)
+const loadMoreBtn = document.querySelector(`.load-more`)
+
+console.log(searchInput);
+console.log(searchBtn);
+ console.log(loadMoreBtn);
